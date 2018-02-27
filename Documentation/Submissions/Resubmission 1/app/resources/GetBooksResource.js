04093ba6719f2bd74599fcc9cb6c51195e27e854
Mattias Wickberg <mattiaswickberg@gmail.com>
2018-02-27 12:48:46 +0100

    preparing submission

---
+++ Documentation/Submissions/Resubmission 1/app/resources/GetBooksResource.js
@@ -0,0 +1,17 @@
+
+(function (getBooks) {
+    "use strict";
+
+    var LibraryDAO = require('../dao/LibraryDAO')
+
+    //import json from file
+    var books = require('../dao/books.json')
+
+    module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
+        //callback(console.log(books))
+
+        //send books with callback function
+        callback(books)
+    };
+
+}());
